882483ba11fe498f5ffc2104258214e03addd8ef
Luiz Henrique <henrique.luiz-sup@univem.edu.br>
2024-03-31 01:44:56 -0300

    feat: creating sample constant

---
+++ src/constants/index.js
@@ -0,0 +1,19 @@
+export const architechtures = [
+    { 
+        name: 'layers', 
+        description: 'lorem ipsum dolor', 
+        pros: ['pro 1', 'pro 2', 'pro 3'], 
+        cons: ['con 1', 'con 2', 'con 3'], 
+        cases: ['case 1', 'case 2', 'case 3'], 
+        conclusion: 'lorem ipsum dolor', 
+        history: 'lorem ipsum dolor',
+        answers: {
+            1: '4.5',
+            2: '6',
+            3: 'medium',
+            4: 'none',
+            5: 'unfamiliar',
+            6: 'no',
+        }
+    },
+]
