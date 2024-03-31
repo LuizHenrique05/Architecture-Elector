79e9adcf3081129d7518fa469523ad2a91a3676d
Luiz Henrique <henrique.luiz-sup@univem.edu.br>
2024-03-31 18:39:05 -0300

    chore: adding target blank

--- src/components/shareds/Bottombar.tsx
+++ src/components/shareds/Bottombar.tsx
@@ -5,7 +5,7 @@ import styles from './Bottombar.module.css'
 function Bottombar() {
   return (
     <footer className={styles.footer}>
-        <h4>&copy; all rights reserved, 2024. Made by: <Link href="https://www.instagram.com/dev.simba/">@dev.simba</Link></h4>
+        <h4>&copy; all rights reserved, 2024. Made by: <Link target='_blank' href="https://www.instagram.com/dev.simba/">@dev.simba</Link></h4>
     </footer>
   )
 }
