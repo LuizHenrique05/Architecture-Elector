9c37c2d82c72fde2d29c8b0dce68d570aae75533
Luiz Henrique <henrique.luiz-sup@univem.edu.br>
2024-03-31 13:02:57 -0300

    chore: filling informations

--- src/lib/actions/architechture.action.ts
+++ src/lib/actions/architechture.action.ts
@@ -5,7 +5,7 @@ import { architechtures } from '@/constants'
 export async function fetchArchitechture(architechtureName: string) {
     if (!architechtureName) return []
 
-    return architechtures.filter(architechture => architechture.name.trim().toLowerCase() === architechtureName.trim().toLowerCase())
+    return architechtures.find(architechture => architechture.name.trim().toLowerCase() === architechtureName.trim().toLowerCase())
 }
 
 export async function fetchArchitechtures() {
